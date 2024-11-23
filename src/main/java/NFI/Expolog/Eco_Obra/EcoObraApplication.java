73c308a97ecb9a5658e2146d67c82f529eafd734
Enos <enoshenrique008@gmail.com>
2024-11-23 20:49:12 -0300

    começo do projeto

---
+++ src/main/java/NFI/Expolog/Eco_Obra/EcoObraApplication.java
@@ -0,0 +1,13 @@
+package NFI.Expolog.Eco_Obra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoObraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoObraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NFI/Expolog/Eco_Obra/EcoObraApplicationTests.java
@@ -0,0 +1,13 @@
+package NFI.Expolog.Eco_Obra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoObraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
